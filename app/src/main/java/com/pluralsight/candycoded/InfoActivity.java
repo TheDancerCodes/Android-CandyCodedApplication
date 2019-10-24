e338c13d452f71dea2f1018acf371c6a9c66933c
Roger Taracha <rojtaracha@gmail.com>
2019-10-24 12:10:30 +0300

    Start the Google Maps Activity: {+} Make createMapIntent(View view).
    {+} Create a Uri for the Address.
    
    {+} Create an ACTION_VIEW Intent.
    
    {+} Set the Intent Package.
    
    {+} Check that an Activity Exists to Handle the Intent.
    
    {+} Start the Activity.
    
    {+} Create TextView Click Handler.

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -1,8 +1,10 @@
 package com.pluralsight.candycoded;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -23,9 +25,24 @@ public class InfoActivity extends AppCompatActivity {
 
     }
 
-    // ***
-    // TODO - Task 2 - Launch the Google Maps Activity
-    // ***
+    /* Create method to Launch the Google Maps Activity
+        & connect method to TextView ClickListener */
+    public void createMapIntent(View view) {
+
+        // Create a URI for the address with a geo string of the address.
+        Uri uriAddress = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
+
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, uriAddress);
+
+        // Make Intent explicit by setting the package to the Google Maps app.
+        mapIntent.setPackage("com.google.android.apps.maps");
+
+        // Check that we have a component that can handle this Intent,
+        // before we start the activity.
+        if (mapIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(mapIntent);
+        }
+    }
 
     // ***
     // TODO - Task 3 - Launch the Phone Activity
